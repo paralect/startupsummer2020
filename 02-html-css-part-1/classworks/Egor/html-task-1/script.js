11ffbb72bf05c12e96d748710887863f7d45084d
Egor <elarayjs@gmail.com>
2020-07-07 17:12:11 +0300

    feat: add html and git task solutions

---
+++ 02-html-css-part-1/classworks/Egor/html-task-1/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  const textarea = document.querySelector('.contact-form_input textarea');
+  const symbolNumber = document.querySelector('.textarea_max-length');
+
+  textarea.addEventListener('keydown', (e) => {
+    symbolNumber.innerHTML = `${textarea.value.length} / 250`
+  })
+}
