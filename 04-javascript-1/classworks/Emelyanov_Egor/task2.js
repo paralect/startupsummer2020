5ceca74e7d05b999b01f74d881da4b5e711da227
Egor <45394619+Elaray1@users.noreply.github.com>
2020-07-09 17:15:12 +0300

    Egor Emelyanov. javascript-1 (#39)
    * feat: add first task solution
    
    * feat: add second task solution
    
    * feat: add examples and rename variables
    
    * refactor: rename variables
    
    * fix: change example 4

---
+++ 04-javascript-1/classworks/Emelyanov_Egor/task2.js
@@ -0,0 +1,91 @@
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  append(data) {
+    const newElement = new Node(data);
+
+    if (this.length) {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newElement;
+    } else {
+      this.head = newElement;
+    }
+    this.length += 1;
+    return this.head;
+  }
+
+  detectloop() {
+    let i = 0;
+    let currentElem = this.head;
+    while (currentElem !== null && i <= this.length) {
+      currentElem = currentElem.next;
+      i += 1;
+    }
+    return i > this.length;
+  }
+
+  detectloopAlt() {
+    let currentElem = this.head;
+    const arr = [];
+    for (let i = 0; i < this.length; i += 1) {
+      if (arr.includes(currentElem.next)) return true;
+      arr.push(currentElem);
+      currentElem = currentElem.next;
+    }
+    return false;
+  }
+
+  addLastNodeToMakeLoop(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+    this.length += 1;
+  }
+}
+
+// Example 1
+const list1 = new LinkedList();
+list1.append(1);
+list1.append(2);
+list1.append(3);
+console.log(list1.detectloopAlt());
+
+// Example 2
+const list2 = new LinkedList();
+list2.append(1);
+list2.append(2);
+list2.append(3);
+list2.addLastNodeToMakeLoop(4);
+console.log(list2.detectloopAlt());
+
+// Example 3
+const list3 = new LinkedList();
+list3.append(1);
+list3.append(2);
+list3.append(3);
+console.log(list3.detectloop());
+
+// Example 4
+const list4 = new LinkedList();
+list4.append(1);
+list4.append(2);
+list4.append(3);
+list4.addLastNodeToMakeLoop(4);
+console.log(list4.detectloop());
+
+module.exports = LinkedList;
