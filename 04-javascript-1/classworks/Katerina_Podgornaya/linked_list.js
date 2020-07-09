9ca8cb842d25232be6a2788581271b816f52881a
Katerina Podgornaya <zzirealy@gmail.com>
2020-07-09 18:04:59 +0300

    Implement JavaScript task 1

--- 04-javascript-1/classworks/Katerina_Podgornaya/linked_list.js
+++ 04-javascript-1/classworks/Katerina_Podgornaya/linked_list.js
@@ -1,130 +1,107 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.repeate = false;
-    }
-}
+const detectLoopInLinkedList = require('./detect_loop');
+const Node = require('./node');
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  addNodeToTheEnd(value) {
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = new Node(value);
     }
 
-    addNodeToTheEnd(value) {
-        let node = new Node(value);
+    this.length += 1;
+  }
 
-        if (this.length == 0) {
-            this.head = node;
-        } else {
-            let currentNode = this.head;
-
-            while (currentNode.next) {
-                currentNode = currentNode.next;
-            }
-
-            currentNode.next = new Node(value);
-        }
-
-        this.length++;
+  addNodeIntoPosition(value, position) {
+    if (position < 0 || position > this.length) {
+      console.log("Sorry, this list doesn't have this position.");
+      return;
     }
 
-    addNodeIntoPosition(value, position) {
-        if (position < 0 | position > this.length) {
-            console.log("Sorry, this list doesn't have this position.")
-            return;
-        }
+    const node = new Node(value);
 
-        let node = new Node(value);
+    if (this.length === 0) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      let previousNode = null;
+      let currentPosition = 0;
 
-        if (this.length == 0) {
-            this.head = node;
-        } else {
-            let currentNode = this.head;
-            let previousNode = null;
-            let currentPosition = 0;
+      while (currentPosition !== position && currentPosition < this.length) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+        currentPosition += 1;
+      }
 
-            while (currentPosition != position && currentPosition < this.length) {
-                previousNode = currentNode;
-                currentNode = currentNode.next;
-                currentPosition++;
-            }
+      previousNode.next = node;
+      node.next = currentNode;
+    }
+    this.length += 1;
+  }
 
-            previousNode.next = node;
-            node.next = currentNode;
-        }
-        this.length++;
+  getNodeByPosition(position) {
+    if (position < 0 || position > this.length) {
+      return "Sorry, this list doesn't have this position.";
     }
 
-    getNodeByPosition(position) {
-        if (position < 0 || position > this.length) {
-            return "Sorry, this list doesn't have this position.";
-        }
+    let currentNode = this.head;
+    let currentPosition = 0;
 
-        let currentNode = this.head;
-        let currentPosition = 0;
-
-        while (currentPosition < position) {
-            currentNode = currentNode.next;
-            currentPosition++;
-        }
-        return currentNode.value;
+    while (currentPosition < position) {
+      currentNode = currentNode.next;
+      currentPosition += 1;
     }
+    return currentNode.value;
+  }
 
-    doLoop() {
-        let currentNode = this.head;
-
-        while (currentNode.next) {
-            currentNode = currentNode.next;
-        }
-
-        currentNode.next = this.head;
-    }
-}
-
-function detectLoopInLinkedList(linkedList) {
-    let loop = false;
-    let currentNode = linkedList.head;
+  doLoop() {
+    let currentNode = this.head;
 
     while (currentNode.next) {
-        console.log("i'm in the loop");
-        if (currentNode.repeate === true) {
-            loop = true;
-            return console.log("true");
-        }
-        currentNode.repeate = true;
-        currentNode = currentNode.next;
+      currentNode = currentNode.next;
     }
-    if (loop === false) {
-        return console.log("false");
-    }
+
+    currentNode.next = this.head;
+  }
 }
 
-//create linked list
-let linkedList = new LinkedList();
+// create linked list
+const linkedList = new LinkedList();
 
-//add nodes
-linkedList.addNodeToTheEnd(1); //1
-linkedList.addNodeToTheEnd(2); //1 --> 2
-linkedList.addNodeToTheEnd(3); //1 --> 2 --> 3
+// add nodes
+linkedList.addNodeToTheEnd(1); // 1
+linkedList.addNodeToTheEnd(2); // 1 --> 2
+linkedList.addNodeToTheEnd(3); // 1 --> 2 --> 3
 
-//add nodes into position
+// add nodes into position
 linkedList.addNodeIntoPosition(1.3, 1); // 1 --> 1.3 --> 2 --> 3
 linkedList.addNodeIntoPosition(1.6, 2); // 1 --> 1.3 --> 1.6 --> 2 --> 3
 
-//add nodes into position not exists
+// add nodes into position not exists
 linkedList.addNodeIntoPosition(1.3, 10); // error
 linkedList.addNodeIntoPosition(1.6, 20); // error
 
-//get node by position
+// get node by position
 console.log(linkedList.getNodeByPosition(2)); // 1.6
 
-//detect if loop in this linked list
-detectLoopInLinkedList(linkedList); //false
+// detect if loop in this linked list
+detectLoopInLinkedList(linkedList); // false
 
-/*create loop linked list*/
+/* create loop linked list */
 linkedList.doLoop();
-detectLoopInLinkedList(linkedList); //true
+detectLoopInLinkedList(linkedList); // true
 
 console.log(linkedList);
