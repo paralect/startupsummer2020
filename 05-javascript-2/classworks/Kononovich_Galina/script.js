68a507585e84602e749648646edcb27881cfdb97
Galina Kononovich <kononovich1703@gmail.com>
2020-07-10 16:49:39 +0300

    add task js-2

--- 05-javascript-2/classworks/Kononovich_Galina/script.js
+++ 05-javascript-2/classworks/Kononovich_Galina/script.js
@@ -1,6 +1,31 @@
-const data = fetch('https://gist.githubusercontent.com/jsturgis/3b19447b304616f18657/raw/a8c1f60074542d28fa8da4fe58c3788610803a65/gistfile1.txt'){
-  .then(response => {
-    
+const fs = require('fs');
+const axios = require('axios');
+// const childProcess = require('child_process');
+
+const extractFrame = require('ffmpeg-extract-frames');
+
+function getData() {
+  const data = axios({
+    method: 'get',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+    responseType: 'stream',
   })
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream('video.mp4'));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return data;
 }
 
+getData();
+const randomNum = (Math.random() * 15000).toFixed();
+
+extractFrame({
+  input: './video.mp4',
+  output: './test3.jpg',
+  offsets: [randomNum],
+});
+
+// childProcess.spawn('ffmpeg -i video.mp4 -ss 00:00:24 -vframes 1 screen2.jpg');
