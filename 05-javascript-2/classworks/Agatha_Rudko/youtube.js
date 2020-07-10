92ef733ed6f17dcc82bd0f5308da6d7d00d2b886
Agatha Rudko <agatharudko@gmail.com>
2020-07-10 14:54:28 +0300

    little code rearrangement

--- 05-javascript-2/classworks/Agatha_Rudko/youtube.js
+++ 05-javascript-2/classworks/Agatha_Rudko/youtube.js
@@ -2,17 +2,6 @@ const fs = require('fs');
 const axios = require('axios');
 const extractFrames = require('ffmpeg-extract-frames');
 
-async function downloadVideo() {
-  axios({
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-    method: 'GET',
-    responseType: 'arraybuffer',
-  }).then((response) => {
-    fs.writeFileSync('videoff.mp4', response.data, (err) => console.log(err));
-  });
-}
-downloadVideo();
-
 async function breakIntoFrames() {
   await extractFrames({
     input: 'videoff.mp4',
@@ -22,4 +11,15 @@ async function breakIntoFrames() {
     ],
   });
 }
-breakIntoFrames();
+
+async function downloadVideo() {
+  axios({
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+    method: 'GET',
+    responseType: 'arraybuffer',
+  }).then((response) => {
+    fs.writeFileSync('videoff.mp4', response.data, (err) => console.log(err));
+    breakIntoFrames();
+  });
+}
+downloadVideo();
