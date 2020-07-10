47e85c99edfcd88b8dfab37db8a57dc15cefa5c6
Agatha Rudko <agatharudko@gmail.com>
2020-07-10 17:28:01 +0300

    randomSec

--- 05-javascript-2/classworks/Agatha_Rudko/youtube.js
+++ 05-javascript-2/classworks/Agatha_Rudko/youtube.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const axios = require('axios');
 const extractFrames = require('ffmpeg-extract-frames');
+const Ffmpeg = require('ffmpeg');
 
+async function randomSec() {
+  const process = new Ffmpeg('videoff.mp4');
+  const video = await process;
+  return video.metadata.duration.seconds * Math.random();
+}
 async function breakIntoFrames() {
   await extractFrames({
     input: 'videoff.mp4',
     output: './screenshot-%i.jpg',
     offsets: [
-      Math.random() * 10000,
+      await randomSec(),
     ],
   });
 }
