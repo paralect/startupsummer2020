738936f1435d4506ec2fd58be627d3a0f9e7ef29
Galina Kononovich <kononovich1703@gmail.com>
2020-07-10 11:37:06 +0300

    Temp

---
+++ 05-javascript-2/classworks/Kononovich_Galina/script.js
@@ -0,0 +1,6 @@
+const data = fetch('https://gist.githubusercontent.com/jsturgis/3b19447b304616f18657/raw/a8c1f60074542d28fa8da4fe58c3788610803a65/gistfile1.txt'){
+  .then(response => {
+    
+  })
+}
+
