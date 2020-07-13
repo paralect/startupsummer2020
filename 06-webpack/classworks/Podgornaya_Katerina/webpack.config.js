82a83edf04ec1cb3e5ca66745db957b4e477b4d5
ZZireal <52997832+ZZireal@users.noreply.github.com>
2020-07-13 18:02:37 +0300

    Katerina Podgornaya.06 webpack (#77)
    * Implement Webpack-task
    
    * It is ok

---
+++ 06-webpack/classworks/Podgornaya_Katerina/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.pcss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpg|ttf|jpeg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.txt$/,
+                use: ['raw-loader']
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'STUDENT': JSON.stringify('Podgornaya Katerina')
+        })
+    ]
+}
+
