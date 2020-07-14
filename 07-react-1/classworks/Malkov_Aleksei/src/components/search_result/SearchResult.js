3b717eb9103b228cd4cc8dfea656ba30d1071bed
dzinrai <alexex.malkov@gmail.com>
2020-07-14 18:09:54 +0300

    fix: implement solution

---
+++ 07-react-1/classworks/Malkov_Aleksei/src/components/search_result/SearchResult.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './search-result.module.css';
+
+
+function SearchResult(props) {
+  function parseImage(src) {
+    const newSrc = src.split('?')[0];
+    return newSrc;
+  }
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={parseImage(props.about.community_icon)} alt={props.about.display_name_prefixed} />
+      <div className={styles.titles}>
+        <span className={styles.result__title}>{props.about.display_name_prefixed}</span>
+        <span className={styles.result__members}>{props.about.subscribers} Members</span>
+      </div>
+  <div className={styles.text}>
+    <span className={styles.text__span}>{props.about.public_description}</span>
+  </div>
+      
+    </div>
+  );
+}
+
+export default SearchResult;
