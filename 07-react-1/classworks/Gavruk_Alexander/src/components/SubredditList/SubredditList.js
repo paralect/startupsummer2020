2527e4ff00db97b6375c462d0817aeac88a258f6
KuzyaCat <lizerd34@mail.ru>
2020-07-14 18:17:06 +0300

    feat: add solution

---
+++ 07-react-1/classworks/Gavruk_Alexander/src/components/SubredditList/SubredditList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SubredditList.css';
+import moment from 'moment';
+
+class SubredditList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getDaysAgo = (created) => {
+    const d = new Date();
+    return Math.round((d - created) / 1000 / 60 / 60 / 24)
+  }
+
+  render() {
+    return (
+      <section className='subreddit-list'>
+        {this.props.data.data.children.map(child => (
+          <div key={child.data.id} className='subreddit-list-item'>
+            <span className='subreddit-list-item_posted'>Posted by u/{child.data.author} {moment.unix(child.data.created).startOf('day').fromNow()}</span>
+            <h2>{child.data.title}</h2>
+            <p>{child.data.selftext}</p>
+            <div className="subreddit-list-item_comment">
+              <img src='./comment.jpg' alt="comment" />
+              <span>{child.data.num_comments} Comments</span>
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  }
+}
+
+export default SubredditList;
