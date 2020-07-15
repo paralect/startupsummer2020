3935aeb79f204aa2ce5fee22c2a7357d3187eb9f
dzinrai <alexex.malkov@gmail.com>
2020-07-15 17:04:36 +0300

    feat: change image to styled component

--- 08-react-2/classworks/Malkov_Aleksei/src/components/search_result/SearchResult.js
+++ 08-react-2/classworks/Malkov_Aleksei/src/components/search_result/SearchResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './search-result.module.css';
+import RoundImage from 'components/round_image';
 
 
 function SearchResult(props) {
@@ -7,17 +8,18 @@ function SearchResult(props) {
     const newSrc = src.split('?')[0];
     return newSrc;
   }
+  console.log(parseImage(props.about.community_icon));
+  //<img className={styles.image} src={parseImage(props.about.community_icon)} alt={props.about.display_name_prefixed} />
   return (
     <div className={styles.container}>
-      <img className={styles.image} src={parseImage(props.about.community_icon)} alt={props.about.display_name_prefixed} />
+      <RoundImage src={parseImage(props.about.community_icon)} alt={props.about.display_name_prefixed} />
       <div className={styles.titles}>
         <span className={styles.result__title}>{props.about.display_name_prefixed}</span>
         <span className={styles.result__members}>{props.about.subscribers} Members</span>
       </div>
-  <div className={styles.text}>
-    <span className={styles.text__span}>{props.about.public_description}</span>
-  </div>
-      
+      <div className={styles.text}>
+        <span className={styles.text__span}>{props.about.public_description}</span>
+      </div>
     </div>
   );
 }
