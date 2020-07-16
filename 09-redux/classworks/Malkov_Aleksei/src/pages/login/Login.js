20e0ab0b8f7080aba46a8cf2b68fbae723bbb2a7
dzinrai <alexex.malkov@gmail.com>
2020-07-16 11:36:38 +0300

    Feat: add prev task to folder

---
+++ 09-redux/classworks/Malkov_Aleksei/src/pages/login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CLIENT_ID = 'fKOjQW4vgv9Ybw'
+
+const AUTH_LINK = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=token&state=${Math.random()}&redirect_uri=${'http://localhost:3000/callback'}&scope=read`;
+
+
+function Login() {
+  return (
+    <a href={AUTH_LINK}>
+      Log in to Reddit
+    </a>
+  );
+}
+
+export default Login;
