9a00ae6d6bd80ffe73c729f7292675513ef28bca
Anna <kvetka99@mail.ru>
2020-07-21 21:57:31 +0300

    Server side of RESTfull API

--- 12-rest/classworks/Volodkova_Anna/index.js
+++ 12-rest/classworks/Volodkova_Anna/index.js
@@ -1,23 +1,22 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
-const publicRouter = new Router();
-const privateRouter = new Router();
 const crypto = require('crypto');
 const jsonwebtoken = require('jsonwebtoken');
 const jwt = require('koa-jwt');
 const  fs = require('fs');
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 const users = JSON.parse(fs.readFileSync('./users.json'));
-const secretKey = 'shhhhh';
+const taglines = JSON.parse(fs.readFileSync('./taglines.json'))
+const SECRETKEY = 'shhhhh';
 
 const app = new koa();
-
-app.use(bodyParser());
+const publicRouter = new Router();
+const privateRouter = new Router();
 
 publicRouter
-  .post('/signUp', async (ctx, next) => {
+  .post('/signUp', async (ctx) => {
     console.log('signUp body', ctx.request.body);
     const logIn = ctx.request.body.logIn;
     const password = ctx.request.body.password;
@@ -31,7 +30,7 @@ publicRouter
     ctx.status = 200;
     ctx.body = 'Okay!';
   })
-  .post('/signIn', (ctx, next) => {
+  .post('/signIn', (ctx) => {
     console.log('signIn body', ctx.request.body);
     const logIn = ctx.request.body.logIn;
     const password = ctx.request.body.password;
@@ -47,70 +46,66 @@ publicRouter
 
     const isLogIn = user.password === passwordHash;
     if (isLogIn) {
-      const token = jsonwebtoken.sign({logIn: logIn}, secretKey);
+      const token = jsonwebtoken.sign({logIn: logIn}, SECRETKEY);
       ctx.status = 200;
       ctx.body = token;
     } else {
       ctx.status = 400;
       ctx.body = 'Incorrect login and password\n';
     }
-  });
+  })
+  .get('/users', (ctx) => {
+    ctx.body = users;
+  })
+  .get('/taglines', (ctx) => {
+    ctx.body = taglines;
+  })
 
-const genArticle = async (prompt) => {
-  try {
-    const res = await axios.post(parfUrl, {
-        prompt,
-        length: 30,
-        num_samples: 1,
-      },
-      {
-        headers: {
-          // 'Host': 'models.dobro.ai',
-          'User-Agent': 'insomnia/2020.3.3',
-          // 'Content-Type': 'application/json',
-          // 'Accept': '*/*',
-          // 'Content-Length': 58
-        },
-        timeout: 10000,
-      });
-    console.log(res.data);
-    return res;
-  } catch(e) {
-    console.log(e)
-  };
-};
-
-
+;
 
 privateRouter
-  //cтатья
-  .post('/article', async (ctx, next) => {
-
-  })
-  //слоган
   .post('/tagline', (ctx, next) => {
-    console.log('tagline', ctx.request.body.arr);
+    console.log('tagline', ctx.request.body);
     ctx.status = 200;
-    ctx.body = ctx.request.body.arr;
+    ctx.body = 'All data received thank you';
+    (async () => {
+      const body = {
+        prompt: ctx.request.body.join(' '),
+        length: 30,
+        num_samples: 1
+      };
 
+      const response = await fetch('https://models.dobro.ai/gpt2/medium/', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/2020.3.3'}
+      });
 
+      const json = await response.json();
+
+      const replies = json.replies;
+
+      const tagline = [...ctx.request.body, ...replies].join(' ');
+
+      taglines.push(tagline);
+      fs.writeFileSync('./taglines.json', JSON.stringify(taglines, null, 2));
+    })();
   })
-  //интервью
-  .post('/tagline', (ctx, next) => {
+  ;
 
-  });
+app.use(bodyParser());
 
 app
   .use(publicRouter.routes())
   .use(publicRouter.allowedMethods());
 
-app.use(jwt({secret: secretKey}));
+app.use(jwt({secret: SECRETKEY}));
 
 app
   .use(privateRouter.routes())
   .use(privateRouter.allowedMethods());
 
-app.listen(3001);
+app.listen(3000);
 
 
 function myCripto(password, salt='salt', iterations = 10000, keylen=64, digest='sha512') {
