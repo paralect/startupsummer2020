cb1ce10790ce2be32a9ac508ad6a82dc37399e49
dzinrai <alexex.malkov@gmail.com>
2020-07-23 16:21:52 +0300

    Fix: change adding values

--- 13-db-1/classworks/Malkov_Aleksei/solution.sql
+++ 13-db-1/classworks/Malkov_Aleksei/solution.sql
@@ -1,41 +1,15 @@
 INSERT INTO flats (id, rooms_amount, flat_number)
-VALUES (1, 4, 10);
-INSERT INTO flats (id, rooms_amount, flat_number)
-VALUES (2, 2, 5);
-INSERT INTO flats (id, rooms_amount, flat_number)
-VALUES (3, 3, 40);
-INSERT INTO flats (id, rooms_amount, flat_number)
-VALUES (4, 2, 10);
+VALUES (1, 4, 10), (2, 2, 5), (3, 3, 40), (4, 2, 10);
 
 INSERT INTO houses (id, street, address)
-VALUES (1, 'Bogdanovicha', '25');
-
-INSERT INTO houses (id, street, address)
-VALUES (2, 'Plehanova', '33');
-
-INSERT INTO houses (id, street, address)
-VALUES (3, 'Pushkina', '10');
-
-INSERT INTO houses (id, street, address)
-VALUES (4, 'Mihailova', '21');
+VALUES (1, 'Bogdanovicha', '25'), (4, 'Mihailova', '21'), (2, 'Plehanova', '33'), (3, 'Pushkina', '10');
 
 INSERT INTO persons (first_name, last_name, age)
-VALUES ('Antoshka', 'Baklanov', 30);
-
-INSERT INTO persons (first_name, last_name, age, house_id, flat_id)
-VALUES ('Kakoyto', 'Chel', 26, 4, 4);
-
-INSERT INTO persons (first_name, last_name, age, house_id, flat_id)
-VALUES ('Valentina', 'Ivanovna', 70, 3, 3);
-
-INSERT INTO persons (first_name, last_name, age, house_id, flat_id)
-VALUES ('Sergei', 'Ekmundovich', 41, 2, 2);
-
-INSERT INTO persons (first_name, last_name, age, house_id, flat_id)
-VALUES ('Natalia', 'Validolovna', 64, 1, 2);
-
-INSERT INTO persons (first_name, last_name, age, house_id, flat_id)
-VALUES ('James', 'Keksikov', 18, 1, 1);
+VALUES ('James', 'Keksikov', 18, 1, 1),
+('Antoshka', 'Baklanov', 30, 5, 5), ('Kakoyto', 'Chel', 26, 4, 4),
+('Valentina', 'Ivanovna', 70, 3, 3),
+('Sergei', 'Ekmundovich', 41, 2, 2),
+('Natalia', 'Validolovna', 64, 1, 2);
 
 
 SELECT first_name, last_name, age FROM persons WHERE age > 50;
@@ -50,6 +24,6 @@ WHERE street = 'Plehanova' AND rooms_amount = 2;
 
 SELECT first_name, last_name, age, street, flat_number
 FROM persons 
-JOIN houses on houses.id = persons.house_id 
-JOIN flats on flats.id = persons.flat_id
+LEFT JOIN houses on houses.id = persons.house_id 
+LEFT JOIN flats on flats.id = persons.flat_id
 WHERE flat_number = 10;
