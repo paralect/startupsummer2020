d3ed673792502c9059d6a9c77fef5c9117f240ba
dzinrai <dzinrai@mail.ru>
2020-07-29 21:17:53 +0300

    Fix: some vars, some spaghetti ifs, static file for error

--- 10-node/classworks/Malkov_Aleksei/index.js
+++ 10-node/classworks/Malkov_Aleksei/index.js
@@ -7,54 +7,62 @@ const rightHash = '37e753db1079f7d8ff8d145769664df46b12d8c3a3c4930a2e66ab76a8bfb
 
 http
   .createServer((req, res) => {
+
     if(req.method === "GET") {
-      if (req.url === '/home.html') {
-        fs.readFile('./home.html', (err, data) => {
-          if (err) throw err;
-          res.setHeader("Content-Type", "text/html");
-          res.write(data);
+
+      switch (req.url) {
+        case '/home.html':
+          fs.readFile('./home.html', (err, data) => {
+            if (err) throw err;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            res.end();
+          });
+        case '/dir_name':
+          res.write(`${__dirname}`);
           res.end();
-        });
-      } else if (req.url === '/dir_name') {
-        res.write(`${__dirname}`);
-        res.end();
-      } else if (req.url === '/file_name') {
-        res.write(`${__filename}`);
-        res.end();
-      } else if (req.url === '/cpus') {
-        res.setHeader("Content-Type", "application/json");
-        const cpus = JSON.stringify(os.cpus());
-        res.write(cpus);
-        res.end();
-      } else if (req.url === '/number_of_cores') {
-        const cpus = os.cpus();
-        res.write(`${cpus.length}`);
-        res.end();
+        case '/file_name':
+          res.write(`${__filename}`);
+          res.end();
+        case '/cpus':
+          res.setHeader("Content-Type", "application/json");
+          res.write(JSON.stringify(os.cpus()));
+          res.end();
+        case '/number_of_cores':
+          res.write(`${os.cpus().length}`);
+          res.end();
       }
+
     } else if (req.method === "POST") {
-      var requestBody = '';
+
+      let requestBody = '';
+      let error;
+
       req.on('data', function(data) {
-        requestBody += data;
-        if(requestBody.length > 1e7) {
+
+        requestBody = requestBody.concat(data);
+        if (requestBody.length > 1e7) {
+          error = true;
           res.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
-          res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
+          fs.readFile('./error413.html', (err, data) => {
+            res.write(data);
+            res.end();
+          });
         }
+
       });
+
       req.on('end', function() {
-        var formData = qs.parse(requestBody);
+        if (error) return;
+        const formData = qs.parse(requestBody);
         const key = crypto.pbkdf2Sync(formData.password, 'salt', 100000, 64, 'sha512');
-        
         res.writeHead(200, {'Content-Type': 'application/javascript'});
-        if (key.toString('hex') === rightHash) {
-          res.write(JSON.stringify({ isValid: true }));
-          res.end();
-        } else {
-          res.write(JSON.stringify({ isValid: false }));
-          res.end();
-        }
+        res.write(JSON.stringify({ isValid: key.toString('hex') === rightHash }));
+        res.end();
+
       });
+
     }
-    
+
   })
   .listen(3000);
-
