7f6084e0753d22abb9c699f4679cebc778cfc2ce
Loi Gloria <loigloria2001@gmail.com>
2020-08-01 23:46:27 +0300

    Refactor, add start script

--- 10-node/classworks/Gloria_Loi/index.js
+++ 10-node/classworks/Gloria_Loi/index.js
@@ -4,62 +4,75 @@ const os = require('os');
 const qs = require('query-string');
 const crypto = require('crypto');
 
-const HASH = '37e753db1079f7d8ff8d145769664df46b12d8c3a3c4930a2e66ab76a8bfb4ca9e0fe69b7bc40355755846342cf19c95c58fa538d964963f04aff409621ec330'
+const HASH =
+  '37e753db1079f7d8ff8d145769664df46b12d8c3a3c4930a2e66ab76a8bfb4ca9e0fe69b7bc40355755846342cf19c95c58fa538d964963f04aff409621ec330';
+const SALT = 'salt';
+const ITERATIONS = 100000;
+const KEYLEN = 64;
+const DIGEST = 'sha512';
 
 const server = http.createServer((req, res) => {
-  if(req.method === "GET"){
-    switch(req.url) {
-      case '/home': {
-        fs.readFile(`${__dirname}/home.html`, (err, content) => {
-          if(!err) {
-            res.setHeader('Content-Type', 'text/html');
-            res.write(content);
-            res.end();
-          } else {
-            res.statusCode = 500;
-            res.write('An error has ocurred');
-            res.end();
-          }
-        })
-      }; 
+  if (req.method === 'GET') {
+    switch (req.url) {
+      case '/home':
+        {
+          fs.readFile(`${__dirname}/home.html`, (err, content) => {
+            if (!err) {
+              res.setHeader('Content-Type', 'text/html');
+              res.write(content);
+              res.end();
+            } else {
+              res.statusCode = 500;
+              res.write('An error has ocurred');
+              res.end();
+            }
+          });
+        }
         break;
-      case '/file_name': { 
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`/home/${__dirname}/${__filename}`);
-        res.end();
-      }; 
+      case '/file_name':
+        {
+          res.setHeader('Content-Type', 'text/html');
+          res.write(`/home/${__filename}`);
+          res.end();
+        }
         break;
-      case '/dir_name':{ 
-        res.write(`/home/${__dirname}`);
-        res.end();
-      }; 
+      case '/dir_name':
+        {
+          res.write(`/home/${__dirname}`);
+          res.end();
+        }
         break;
-      case '/cpus':{
-        res.write(`${os.cpus()}`);
-        res.end();}; 
+      case '/cpus':
+        {
+          res.write(`${os.cpus()}`);
+          res.end();
+        }
         break;
-      case '/number_of_cores':{ 
-        res.write(`${os.cpus().length}`);
-        res.end();}; 
+      case '/number_of_cores':
+        {
+          res.write(`${os.cpus().length}`);
+          res.end();
+        }
         break;
-      default: {      
+      default: {
         res.write('Cannot handle this request');
         res.end();
       }
     }
   }
-  if(req.method === "POST"){
-    let body = ''
-    req.on('data', data => {
-      body+=data;
-    })
+  if (req.method === 'POST') {
+    let body;
+    req.on('data', (data) => {
+      body += data;
+    });
     req.on('end', () => {
-      const key = crypto.pbkdf2Sync(qs.parse(body).password, 'salt', 100000, 64, 'sha512').toString('hex');
-        res.write(`${ key === HASH }`);
-        res.end();
-    })
-
+      const key = crypto
+        .pbkdf2Sync(qs.parse(body).password, SALT, ITERATIONS, KEYLEN, DIGEST)
+        .toString('hex');
+      res.write(`${key === HASH}`);
+      res.end();
+    });
   }
-})
+});
 
-server.listen(8000, console.log("server started in localhost:8000"));
+server.listen(8000, console.log('server started in localhost:8000'));
