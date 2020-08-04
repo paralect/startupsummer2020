ab471c7fd490176f0c12143ccd5ced6d1acb8082
dzinrai <alexex.malkov@gmail.com>
2020-08-04 11:36:36 +0300

    Fix passthrough for jwt

--- 12-rest/classworks/Malkov_Alexei/signin.js
+++ 12-rest/classworks/Malkov_Alexei/signin.js
@@ -1,4 +1,5 @@
 const checkUser = require("./checkUser");
+const secret = 'shared-secret';
 
 function signin(ctx, jwt) {
   const formData = ctx.request.body;
@@ -6,8 +7,11 @@ function signin(ctx, jwt) {
       ctx.status = 200;
       const link = "http://localhost:3000/posts";
       const page = `<html><head></head><body><h1>Hello, ${formData.name}</h1><br><a href="${link}">posts</a></body></html>`;
-      jwt.sign({foo: 'bar'}, 'shared-secret');
-      ctx.body = page;
+      const token = jwt.sign({foo: 'bar'}, secret);
+      ctx.session.token = token;
+      ctx.body = {
+        token,
+      };
       return { name: formData.name, ok: true }
     }
     return { name: formData.name, ok: false }
