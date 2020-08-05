172f8b13e723252765aea9b238e6beac979742bc
Galina Kononovich <kononovich1703@gmail.com>
2020-08-05 15:19:23 +0300

    add changes with routers and session

--- 11-node-frameworks/KononovichGalina/index.js
+++ 11-node-frameworks/KononovichGalina/index.js
@@ -1,55 +1,70 @@
-const koa = require('koa');
-const router = require('koa-router');
+const Koa = require('koa');
+const Router = require('koa-router');
+const Joi = require('@hapi/joi');
 const serve = require('koa-static');
-const app = new koa();
+const json = require('koa-json');
+const bodyParser = require('koa-bodyparser');
 const session = require('koa-generic-session');
-const init = router();
-// const KoaBody = require('koa-body');
-
-// koaBody = convert(KoaBody());
-
-// init.get('/get', getData);
-
-// function getData(ctx, next) {
-//   console.log('nfj');
-//   ctx.body = 'get data';
-//   next();
-// }
-
-// app.use(init.routes());
 
 
-app.use(serve(__dirname + '/public'));
-
-app.use(async(ctx, next) => {
-  ctx.body = 'get data';
-  await next();
-});
-
-app.use(async(ctx, next) => {
-  ctx.body = 'change data';
-  await next();
-});
-
+const app = new Koa();
+const router = new Router();
+let db = [];
 app.keys = ['Shh, its a secret!'];
+
+const schema = Joi.object({
+  firstName: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+  
+  lastName: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+
+  description: Joi.string()
+      .min(3)
+      .required(),
+  
+  mark: Joi.string()
+      .required()
+});
+
+router.post('/quiz', async (ctx, next) => {
+  try {
+    const answer = await schema.validateAsync(ctx.request.body);
+    db.push(answer);
+    ctx.response.body = JSON.stringify({answer: 'Thanks for your answer!'});
+  }
+  catch (err) { 
+    ctx.response.body = JSON.stringify({error: err});
+    console.log(err);
+  }
+});
+
+router.get('/counter', async (ctx, next) => {
+  ctx.response.body = `Counter = ${ctx.session.views}`;
+});
+
+router.get('/answers', async ctx => {
+  ctx.response.body = JSON.stringify({user: db});
+});
+
 app.use(session(app));
-app.use(async(ctx, next) => {
+app.use(bodyParser());
+app.use(json());
+
+
+app.use(serve('index'));
+app.use((ctx, next) => {
   let count = ctx.session.views || 0;
   ctx.session.views = ++count;
-  ctx.body = count + 'views';
   next();
-});
+} )
 
-// init.post('/', async (ctx, next) => {
-//   console.log('lala');
-// });
-
-app.use(async(ctx) => {
- if(ctx.method === 'POST') {
-   console.log(ctx.request.body); //bodyParser
- }
-});
-
-// app.use(serve(__dirname + '/public'));
+app
+.use(router.routes())
+.use(router.allowedMethods());
 
 app.listen(3000);
