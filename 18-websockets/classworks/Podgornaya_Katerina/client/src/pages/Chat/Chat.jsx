d22d7ed59b1d06a145c3701a035316a500bd3b19
ZZireal <zzirealy@gmail.com>
2020-08-06 13:37:40 +0300

    Add logic for chating between sockets

--- 18-websockets/classworks/Podgornaya_Katerina/client/src/pages/Chat/Chat.jsx
+++ 18-websockets/classworks/Podgornaya_Katerina/client/src/pages/Chat/Chat.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-import io from 'socket.io-client';
 
 import './Chat.css';
 import Message from './components/Message';
 
+const io = require("socket.io-client");
+const ioClient = io.connect("http://localhost:3031");
+
 function Chat() {
-  const io = require("socket.io-client");
-  const ioClient = io.connect("http://localhost:3030");
-
+  const [username, setUsername] = useState('Anonymous');
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  
   const sendMessage = () => {
-    ioClient.emit('send-message');
-  }
+    const msg = {
+      message: inputMessage,
+      username
+    };
+    setMessages((messages) => [...messages, msg]);
+    ioClient.emit('send-message', msg);
+  };
 
-  const [message, setMessage] = useState(null);
+  React.useEffect(() => {
+    ioClient.on('get-message', (msg) => {
+      setMessages((messages) => [...messages, msg]);
+    }
+  );
+  }, []);
 
   return (
     <div className="chat">
@@ -22,13 +35,29 @@ function Chat() {
 
       <section>
         <div id="change_username">
-          <input id="username" type="text"/>
-          <button id="send_username" type="button">Change username</button>
+          <input 
+            id="username" 
+            type="text" 
+            name='message'
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <button 
+            id="send_username" 
+            type="button"
+            onClick={() => setUsername(username)}
+           >
+            Change username
+          </button>
         </div>
       </section>
 
       <section id="chatroom">
-        <Message username="Ivan" message="Help me" />
+        {
+          messages.map((message) => (
+            <Message username={message.username} message={message.message} />
+          ))
+        }
         <section id="feedback"></section>
       </section>
       
@@ -37,13 +66,16 @@ function Chat() {
           id="message" 
           className="vertical-align" 
           type="text"
+          onChange={(e) => setInputMessage(e.target.value)}
+          value={inputMessage}
         />
         <button 
           id="send_message" 
           className="vertical-align" 
           type="button"
-          onClick={() => sendMessage()}
-        >Send</button>
+          onClick={sendMessage}
+        >
+          Send</button>
       </section>
     </div>
   );
--- 18-websockets/classworks/Podgornaya_Katerina/server/index.js
+++ 18-websockets/classworks/Podgornaya_Katerina/server/index.js
@@ -1,14 +1,28 @@
-const io = require("socket.io");
+const io = require('socket.io');
 
-const server = io.listen(3030);
+const server = io.listen(3031);
+
+const FgBlack = '\x1b[30m';
+const FgRed = '\x1b[31m';
+const FgGreen = '\x1b[32m';
+const FgYellow = '\x1b[33m';
+const FgBlue = '\x1b[34m';
+const FgMagenta = '\x1b[35m';
+const FgCyan = '\x1b[36m';
+const FgWhite = '\x1b[37m';
 
 server.on('connection', (socket) => {
-  console.log(`Client connected id = ${socket.id}`);
+  console.log(`${FgBlue}New connection!\n${FgGreen}Client connected id = ${socket.id.slice(0, 3)}`);
+
+  socket.on('set-username', (username) => {
+    console.log(`${FgGreen}Client (id = ${socket.id.slice(0, 3)}) set username '${username}'`);
+  });
+  socket.on('send-message', (message) => {
+    console.log(`${FgYellow}Client (id = ${socket.id.slice(0, 3)}) send some message${FgWhite}: ${message.message}`);
+    socket.broadcast.emit('get-message', message);
+  });
 
   socket.on('disconnect', () => {
-    console.log(`Client gone id = ${socket.id}`);
-  });
-  socket.on('send-message', () => {
-    console.log('client send some message');
+    console.log(`${FgRed}Client gone id = ${socket.id.slice(0, 3)}${FgWhite}`);
   });
 });
