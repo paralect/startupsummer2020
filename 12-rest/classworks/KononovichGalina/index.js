479bdc618380e9e8f0886e61680b134d01a7c91e
Galina Kononovich <kononovich1703@gmail.com>
2020-08-10 14:51:55 +0300

    add slogans

--- 12-rest/classworks/KononovichGalina/index.js
+++ 12-rest/classworks/KononovichGalina/index.js
@@ -3,28 +3,37 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
-const fetch = require("fetch");
+const axios = require('axios').default;
 const url = 'https://porfirevich.ru';
-
+const fs = require('fs');
 const app = new koa();
-const router = new Router();
+const public = new Router();
+const privat = new Router();
 
 app.use(bodyParser());
 
 let data = [];
+// let slogans=[];
 function getPasswordHash(allData) {
   const key = crypto.pbkdf2Sync(allData.password, 'salt', 100000, 64, 'sha512').toString('hex');
-  user ={hash: key, password: allData.password, login:allData.login};
+  user ={hash: key, login:allData.login};
   return user;
 } 
 
-router.post('/reg', async (ctx) => {
-  ctx.body = ctx.request.body;
+public.post('/reg', async (ctx) => {
+  ctx.response.body = 'Thank you for registration!';
   data.push(getPasswordHash(ctx.request.body));
+  const usersJson = JSON.stringify(data);
+  fs.writeFile("users.json", usersJson, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
 });
 
 
-router.post('/login', async (ctx) => {
+public.post('/login', async (ctx) => {
   ctx.body = ctx.request.body;
   const currentUser = getPasswordHash(ctx.request.body)
 
@@ -33,34 +42,46 @@ router.post('/login', async (ctx) => {
       return item;
     };
   });
-
  if(flag) {
   const secret = 'secret';
   const token = jwt.sign(flag, secret);
-  ctx.body = token;
-  console.log(token);
-
-
-  const phrases= "fgfg";
-  const response = await fetch(url, {
-    method: 'POST', 
-    body: JSON.stringify(phrases), 
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'insomnia/2020.3.3',
-    }
-  })
-  .then(res => res.json());
-
-  // const json = await response.json();
-  // console.log('str', JSON.stringify(json));
-
+  ctx.body = 'You are authorized';
  }
+ else {
+   ctx.body = 'Your login/password is wrong, try again.';
+ }
+});
+
+const slogansCollection = JSON.parse(fs.readFileSync('slogans.json'));
+privat.post('/slogans', async (ctx)  => {
+  const phrase = ctx.request.body;
+  const generatedSlogan = await axios.post('https://models.dobro.ai/gpt2/medium/', {
+    prompt: JSON.stringify(phrase),
+    length: 5,
+    num_samples: 1,
+  }, {
+    headers: { 'User-Agent': 'insomnia/2020.3.3' },
+  });
+
+  ctx.body = { 
+    data: generatedSlogan.data.replies,
+  };
+
+  slogansCollection.slogans.push({phrase, replies: generatedSlogan.data.replies});
+
+  fs.writeFile("slogans.json", JSON.stringify(slogansCollection, null, 2), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+  console.log("The file was saved!");
+  });
 });
 
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .use(public.routes())
+  .use(public.allowedMethods())
+  .use(privat.routes())
+  .use(privat.allowedMethods());
 
 app.listen(3000);
