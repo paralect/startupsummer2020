29f44e1c2a3ef70fd9ff407d29d35928f293a84d
ZZireal <zzirealy@gmail.com>
2020-08-13 12:46:23 +0300

    Beginning

--- 09-redux/classworks/Podgornaya_Katerina/src/resources/store.js
+++ 09-redux/classworks/Podgornaya_Katerina/src/resources/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-export default createStore(reducer, compose(
+const store = createStore(reducer, compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f, // eslint-disable-line
 ));
+
+export default store;
