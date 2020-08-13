651fbefc77992f9c8b3d505fc421905d7f91e7ca
ZZireal <zzirealy@gmail.com>
2020-08-13 11:50:37 +0300

    Beginning

--- 09-redux/classworks/Podgornaya_Katerina/src/App.js
+++ 09-redux/classworks/Podgornaya_Katerina/src/App.js
@@ -1,8 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import Pages from 'pages';
+import { createStore } from 'redux';
+import { Provider, useDispatch } from 'react-redux';
+import Pages from './pages';
 import logo from './images/reddit_logo.svg';
 import search from './images/search_icon.svg';
 import styles from './index.module.css';
+
+const dispatch = useDispatch();
+
+const initialState = {
+  phrase: '',
+};
+
+const changePhrase = (phrase) => ({
+  type: 'ACTION_CHANGE_PHRASE',
+  payload: phrase,
+});
+
+const phraseReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ACTION_CHANGE_PHRASE': {
+      return { ...state, phrase: action.payload };
+    }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(phraseReducer);
 
 function App() {
   const [phrase, setPhrase] = useState(null);
@@ -15,13 +40,16 @@ function App() {
   };
 
   return (
+    <Provider store={store}>
     <main>
       <header className={styles.header}>
         <div className={styles.header_top}>
           <img src={logo}></img>
           <div className={styles.searchField}>
             <img className={styles.searchSvg} src={search} />
-            <input className={styles.input} placeholder='Search' onKeyDown={onClickEnter} />
+            <input className={styles.input} placeholder='Search' onKeyDown={onClickEnter} onChange={(event) => {
+              dispatch(changePhrase(event.target.value));
+            }}/>
           </div>
         </div>
         <div className={styles.header_bot}></div>
@@ -30,6 +58,7 @@ function App() {
         <Pages phrase={phrase} />
       </section>
     </main>
+    </Provider>
   );
 }
 
