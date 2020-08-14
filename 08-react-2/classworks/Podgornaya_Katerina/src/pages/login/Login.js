913b06b27b2bd1fef5bc2ebc0c19442764504583
ZZireal <zzirealy@gmail.com>
2020-08-14 19:35:37 +0300

    real last commit

---
+++ 08-react-2/classworks/Podgornaya_Katerina/src/pages/login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './login_style.module.css';
+
+const CLIENT_ID = '_VNVZ2Bb0tAY3A'
+
+const AUTH_LINK = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=token&state=${Math.random()}&redirect_uri=${'http://localhost:3000/callback'}&scope=read`;
+
+
+function Login() {
+  return (
+    <a href={AUTH_LINK} className={styles.login}>
+      Log in to Reddit
+    </a>
+  );
+}
+
+export default Login;
