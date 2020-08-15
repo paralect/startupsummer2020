9b91df631eaacbb46862d04838b572c114a313fe
ZZireal <zzirealy@gmail.com>
2020-08-15 16:51:03 +0300

    Remove extra code

--- 09-redux/classworks/Podgornaya_Katerina/src/App.js
+++ 09-redux/classworks/Podgornaya_Katerina/src/App.js
@@ -1,33 +1,10 @@
 import React, { useState } from 'react';
-import { Provider, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Pages from './pages';
 import logo from './images/reddit_logo.svg';
 import search from './images/search_icon.svg';
 import styles from './index.module.css';
-import store from './resources/store';
-import getPhrase from './resources/phrase/phrase.selector';
 import phraseAction from './resources/phrase/phrase.action';
-
-// const initialState = {
-//   phrase: '',
-// };
-
-// const changePhrase = (phrase) => ({
-//   type: 'ACTION_CHANGE_PHRASE',
-//   payload: phrase,
-// });
-
-// const phraseReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'ACTION_CHANGE_PHRASE': {
-//       return { ...state, phrase: action.payload };
-//     }
-//     default:
-//       return state;
-//   }
-// };
-
-// const store = createStore(phraseReducer);
 
 function App() {
   const dispatch = useDispatch();
@@ -40,7 +17,6 @@ function App() {
   };
 
   return (
-    <Provider store={store}>
     <main>
       <header className={styles.header}>
         <div className={styles.header_top}>
@@ -56,7 +32,6 @@ function App() {
         <Pages />
       </section>
     </main>
-    </Provider>
   );
 }
 
