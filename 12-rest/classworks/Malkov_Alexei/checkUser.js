06424dc5fbc5451935b37d66488e066eb67c6cc0
dzinrai <alexex.malkov@gmail.com>
2020-08-15 16:36:42 +0300

    Fix missing comma and remove variable

--- 12-rest/classworks/Malkov_Alexei/checkUser.js
+++ 12-rest/classworks/Malkov_Alexei/checkUser.js
@@ -3,17 +3,16 @@ const crypto = require('crypto');
 
 function checkUser(user, withPassword=false) {
   const base = readJSON('base.json');
-  let isExist = false;
   base.users.forEach(userData => {
     if (userData.name === user.name) {
       const key = crypto.pbkdf2Sync(user.password, 'salt', 100000, 64, 'sha512');
       if (!withPassword) isExist = true;
       else if (userData.password === key.toString('hex')) {
-        isExist = true;
+        return true;
       }
     }
   });
-  return isExist;
+  return false;
 }
 
 module.exports = checkUser;
