2a24ae64be423ee3ba21a76faec06505b4346e71
dzinrai <dzinrai@mail.ru>
2020-08-16 17:50:34 +0300

    Fix bug with api

--- 17-react-native/classworks/Malkov_Aleksei/fetchMarvel.js
+++ 17-react-native/classworks/Malkov_Aleksei/fetchMarvel.js
@@ -6,8 +6,8 @@ const privateKey = 'a2d0927ae7143a3ab8c7b7b6b6993b3b27250fe4';
 const publicKey = 'b3880a065a6984f95c2e51cdbdc9cc5a';
 
 const fetchMarvel = (url, params) => {
-  const date = new Date();
-  const timestamp = date.toString();
+  const date = Number(new Date());
+  const timestamp = date;
 
   return axios.get(`${apiUrl}${url}`, {
     params: {
