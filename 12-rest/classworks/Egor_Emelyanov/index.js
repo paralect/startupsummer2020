b462039c76a34230c7349411ed7768bf2cd9f8d7
Egor <5642452@mail.ru>
2020-08-16 21:06:55 +0300

    Fix mistakes 1/2

--- 12-rest/classworks/Egor_Emelyanov/index.js
+++ 12-rest/classworks/Egor_Emelyanov/index.js
@@ -14,7 +14,7 @@ const db = {};
 
 const app = new Koa();
 const publicRoutes = router();
-const privateRouter = router();
+const privateRoutes = router();
 const schema = joi.object({
   login: joi.string().required(),
   password: joi.string().required(),
@@ -22,21 +22,29 @@ const schema = joi.object({
 const interviewSchema = joi.array().items(joi.string().pattern(/\?$/)).min(4);
 const articleSchema = joi.array().items(joi.string()).min(3);
 
+const createPassword = (login, password) => {
+  return db[login] = crypto.pbkdf2Sync(password, 'salt', 100000, 64, 'sha512').toString('hex');
+}
+
+const checkPassword = (login, password) => {
+  return db[login] === crypto.pbkdf2Sync(password, 'salt', 100000, 64, 'sha512').toString('hex');
+}
+
 app.use(bodyParser());
 
 app.use(publicRoutes.routes());
 
 publicRoutes.post('/signup', async (ctx) => {
-  schema.validate(ctx.request.body);
-  db[ctx.request.body.login] = crypto.pbkdf2Sync(ctx.request.body.password, 'salt', 100000, 64, 'sha512').toString('hex');
+  const data = schema.validate(ctx.request.body);
+  createPassword(data.login, data.password);
   ctx.body = {};
 });
 
 publicRoutes.post('/signin', async (ctx) => {
-  schema.validate(ctx.request.body);
-  if (db[ctx.request.body.login] === crypto.pbkdf2Sync(ctx.request.body.password, 'salt', 100000, 64, 'sha512').toString('hex')) {
+  const data = schema.validate(ctx.request.body);
+  if (checkPassword(data.login, data.password)) {
     ctx.body = jwt.sign({
-      login: ctx.request.body.login,
+      login: data.login,
     }, secret);
   } else {
     ctx.status = 401;
@@ -46,31 +54,67 @@ publicRoutes.post('/signin', async (ctx) => {
 
 app.use(koaJwt({ secret }));
 
-app.use(privateRouter.routes());
+app.use(privateRoutes.routes());
 
-privateRouter.post('/setdata/interview', async (ctx) => {
-  interviewSchema.validate(ctx.request.body);
+privateRoutes.post('/interview', async (ctx) => {
+  const data = interviewSchema.validate(ctx.request.body);
   const obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-  ctx.request.body.forEach(async (el) => {
-    const info = await fetch('https://models.dobro.ai/gpt2/medium/', { method: 'POST', headers: { 'User-Agent': 'insomnia/2020.3.3' }, body: JSON.stringify({ prompt: el, length: 30, num_samples: 1 }) }).then((data) => data.json());
+  data.forEach(async (el) => {
+    const info = await fetch(
+      'https://models.dobro.ai/gpt2/medium/',
+      {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'insomnia/2020.3.3',
+        },
+        body: JSON.stringify({
+          prompt: el,
+          length: 30,
+          num_samples: 1
+        }),
+      }).then((data) => data.json());
     obj.interview = { ...obj.interview, el: info.replies[0] };
   });
 });
 
-privateRouter.post('/setdata/articles', async (ctx) => {
-  articleSchema.validate(ctx.request.body);
+privateRoutes.post('/articles', async (ctx) => {
+  const data = articleSchema.validate(ctx.request.body);
   const obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-  ctx.request.body.forEach(async (el) => {
-    const info = await fetch('https://models.dobro.ai/gpt2/medium/', { method: 'POST', headers: { 'User-Agent': 'insomnia/2020.3.3' }, body: JSON.stringify({ prompt: el, length: 20, num_samples: 1 }) }).then((data) => data.json());
+  data.forEach(async (el) => {
+    const info = await fetch(
+      'https://models.dobro.ai/gpt2/medium/',
+      {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'insomnia/2020.3.3',
+        },
+        body: JSON.stringify({
+          prompt: el,
+          length: 20,
+          num_samples: 1,
+        }),
+      }).then((data) => data.json());
     obj.articles = { ...obj.articles, el: info.replies[0] };
   });
 });
 
-privateRouter.post('/setdata/tagline', async (ctx) => {
-  articleSchema.validate(ctx.request.body);
+privateRoutes.post('/tagline', async (ctx) => {
+  const data = articleSchema.validate(ctx.request.body);
   const obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-  ctx.request.body.forEach(async (el) => {
-    const info = await fetch('https://models.dobro.ai/gpt2/medium/', { method: 'POST', headers: { 'User-Agent': 'insomnia/2020.3.3' }, body: JSON.stringify({ prompt: el, length: 3, num_samples: 1 }) }).then((data) => data.json());
+  data.forEach(async (el) => {
+    const info = await fetch(
+      'https://models.dobro.ai/gpt2/medium/',
+      {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'insomnia/2020.3.3'
+        },
+        body: JSON.stringify({
+          prompt: el,
+          length: 3,
+          num_samples: 1,
+        }),
+      }).then((data) => data.json());
     obj.tagline = { ...obj.tagline, el: info.replies[0] };
   });
 });
