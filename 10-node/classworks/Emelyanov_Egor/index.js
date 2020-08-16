4fba1f3e1f778f07af6c481feb4df985154be79e
Egor <5642452@mail.ru>
2020-08-16 21:22:40 +0300

    Fix spaghetti code

--- 10-node/classworks/Emelyanov_Egor/index.js
+++ 10-node/classworks/Emelyanov_Egor/index.js
@@ -7,26 +7,7 @@ const hash = crypto.pbkdf2Sync('supper-secure-password', 'salt', 100000, 64, 'sh
 const checkHash = (obj) => crypto.pbkdf2Sync(obj.password, 'salt', 100000, 64, 'sha512').toString('hex') === hash;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/home') {
-    fs.readFile(`${__dirname}/home.html`, (err, content) => {
-      if (!err) {
-        res.setHeader('Content-Type', 'text/html');
-        res.write(content);
-      } else {
-        res.statusCode = 500;
-        res.write('An error has ocurred');
-      }
-      res.end();
-    });
-  } else if (req.url === '/dir_name' && req.method === 'GET') {
-    console.log(__dirname);
-  } else if (req.url === '/file_name' && req.method === 'GET') {
-    console.log(__filename);
-  } else if (req.url === '/cpus' && req.method === 'GET') {
-    console.log(os.cpus());
-  } else if (req.url === '/number_of_cores' && req.method === 'GET') {
-    console.log(os.cpus().length);
-  } else if (req.method === 'POST') {
+  if (req.method === 'POST') {
     let data = '';
     let isValid = false;
     req.on('data', (chunk) => {
@@ -38,6 +19,40 @@ const server = http.createServer((req, res) => {
       res.write(JSON.stringify({ isValid }));
       res.end();
     });
+  } else if (req.method === 'GET') {
+    switch (req.url) {
+      case '/home':
+        fs.readFile(`${__dirname}/home.html`, (err, content) => {
+          if (!err) {
+            res.setHeader('Content-Type', 'text/html');
+            res.write(content);
+          } else {
+            res.statusCode = 500;
+            res.write('An error has ocurred');
+          }
+          res.end();
+        });
+        break;
+  
+      case '/dir_name':
+        console.log(__dirname);
+        break;
+  
+      case '/file_name':
+        console.log(__filename);
+        break;
+  
+      case '/cpus':
+        console.log(os.cpus());
+        break;
+  
+      case '/number_of_cores':
+        console.log(os.cpus().length);
+        break;
+    
+      default:
+        break;
+    };
   }
 });
 
