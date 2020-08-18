f74b95a6592fa19d6bdf16f0d88de2022e6dcc2d
Galina Kononovich <kononovich1703@gmail.com>
2020-08-18 11:32:02 +0300

    fix bags

--- 10-node/classworks/KononovichGalina/index.js
+++ 10-node/classworks/KononovichGalina/index.js
@@ -1,42 +1,57 @@
 const http = require('http');
 const fs = require('fs');
 const os = require('os');
-const request = require('request');
-const { Console } = require('console');
 const crypto = require('crypto');
 
 
 const server = http.createServer((req, res) => {
 
-  if(req.method === 'POST') {
-    const key = crypto.pbkdf2Sync(req.headers.password , 'salt', 100000, 64, 'sha512').toString('hex') ;
-    if(key === '37e753db1079f7d8ff8d145769664df46b12d8c3a3c4930a2e66ab76a8bfb4ca9e0fe69b7bc40355755846342cf19c95c58fa538d964963f04aff409621ec330'){
-      res.write(JSON.stringify({ isValid: true }));
+  const getHomePage = (res) => {
+    console.log('GET');
+    fs.readFile(`${__dirname}/home.html`, (err, content) => {
+      if(!err) {
+          res.setHeader('Content-Type', 'text/html');
+          res.write(content);
+          res.end();
+      } 
+  })}
+
+  const initGetRoutes = (req, res) => {
+    switch (req.url) {
+      case '/home': 
+      getHomePage(res);
+      break;
+      case '/dir_name': 
+      console.log(__dirname);
+      break;
+      case '/file_name': 
+      console.log(__dirname+ '/home.html');
+      break;
+      case '/cpus': 
+      console.log(os.cpus());
+      break;
+      case '/number_of_cores': 
+      console.log(os.cpus().length);
+      break;
     }
-    else {
-      res.write(JSON.stringify({ isValid: false }));
-    }
-    console.log(key);
-  }
-  
-  switch (req.url) {
-    case '/dir_name': console.log(__dirname);
-    case '/file_name': console.log(__dirname+ '/home.html');
-    case '/cpus': console.log(os.cpus());
-    case '/number_of_cores': console.log(os.cpus().length);
   }
 
-  if(req.url === '/home') {
-    if(req.method === 'GET') {
-      console.log('GET');
-    fs.readFile(`${__dirname}/home.html`, (err, content) => {
-        if(!err) {
-            res.setHeader('Content-Type', 'text/html');
-            res.write(content);
-            res.end();
-        } 
-    })}
+  const initPostRoutes = (req, res) => {
+    const key = crypto.pbkdf2Sync(req.headers.password , 'salt', 100000, 64, 'sha512').toString('hex') ;
+    const hash = '37e753db1079f7d8ff8d145769664df46b12d8c3a3c4930a2e66ab76a8bfb4ca9e0fe69b7bc40355755846342cf19c95c58fa538d964963f04aff409621ec330';
+    key === hash? res.write(JSON.stringify({ isValid: true })) : res.write(JSON.stringify({ isValid: false }));
+    res.end();
   }
+ 
+  const routes = {
+    get: initGetRoutes(req, res),
+    post: initPostRoutes(req, res),
+  }
+
+  switch(req.method) {
+    case 'GET': routes.get;
+    case 'POST': routes.post;
+}
 });
 
 server.listen(8080);
