40a26734dfdce39fd0d1c59bf44fcc07f8bff104
Kononovich Halina <kononovich1703@gmail.com>
2020-08-18 08:26:14 +0300

    changes with comments list

--- 07-react-1/classworks/KononovichGalina/example/src/components/Item/Item.js
+++ 07-react-1/classworks/KononovichGalina/example/src/components/Item/Item.js
@@ -41,48 +41,5 @@ const Item = (props) => {
 }
 
 export default Item;
-  // return (
-  //   <div className="item">
-  //     <div className="authorContainer">
-  //       {'Posted by ' + props.item.data.author_fullname}
-  //     </div> 
-  //     <div className="headerItem">
-  //       {props.item.data.title}
-  //     </div>
-  //     <div className="bodyItem">
-  //       {props.item.data.selftext}
-  //     </div>
-
-  //     <div className="comments">
-  //       <img src={iconComment}/> 
-  //       <div className="countComments">
-  //           {props.item.data.num_comments + ' Comments'}
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
-
-  // else {
-  //   return (
-  //     <div className="item itemSearch" onClick={() => props.clickHandler(props.inputValue)}>
-  //       {/* <div className="infoAbout"> */}
-  //         <img className="communityIcon" src={props.item.data.header_img}></img>
-  //         {/* </div> */}
-  //         <div className="infoAbout">
-  //           <div className="headerCommunity" >
-  //             {props.item.data.display_name_prefixed}
-  //           </div>
-
-  //           <div className="subscribers">
-  //           {props.item.data.subscribers + ' Members'}
-  //           </div>
-            
-  //       </div>
-  //       <div className="description">
-  //         {props.item.data.public_description}
-  //       </div>
-  //     </div>
-  //   )
-  // }
 
 
