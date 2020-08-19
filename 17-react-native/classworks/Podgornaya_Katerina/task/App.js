8201940961fe7f7e59c703162b86665b0396b7ff
ZZireal <zzirealy@gmail.com>
2020-08-19 18:00:57 +0300

    Work in process

--- 17-react-native/classworks/Podgornaya_Katerina/task/App.js
+++ 17-react-native/classworks/Podgornaya_Katerina/task/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as ReduxProvider } from 'react-redux';
-//import { SafeAreaProvider } from 'react-native-safe-area-context'
 import fetchMarvel from './fetchMarvel';
 import store from './resources/store';
 
