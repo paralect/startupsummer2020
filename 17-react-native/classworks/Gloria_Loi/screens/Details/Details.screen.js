0f2ad1de66b9882dcb0e6a6d7892707793a3f631
Loi Gloria <loigloria2001@gmail.com>
2020-08-22 22:32:49 +0300

    Finish application

--- 17-react-native/classworks/Gloria_Loi/screens/Details/Details.screen.js
+++ 17-react-native/classworks/Gloria_Loi/screens/Details/Details.screen.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { Text, View, Image, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRoute } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import fetchMarvel from '../../fetchMarvel';
-
+import {
+  fetchComics,
+  toggleFav,
+} from '../../resources/characters/characters.actions';
+import { getComics } from '../../resources/characters/characters.selectors';
+const logo = require('../../assets/logo.png');
 import styles from './Details.styles';
 
 const DetailsScreen = () => {
   const { params } = useRoute();
-  const { id, description, name, thumbnail } = params;
-  const [comics, setComics] = useState();
+  const { id, description, name, thumbnail, isFav } = params;
+  const [isFavorite, setIsFavorite] = useState(isFav);
 
-  const fetchData = useCallback(async () => {
-console.log(id)
-    const { data } =  await fetchMarvel(`/characters/${id}/comics`);
-    setComics(data.data.results);
-  }, []);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchData();
+    dispatch(fetchComics(id));
   }, []);
+
+  const comics = useSelector(getComics);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image style={styles.header} source={require('../../assets/logo.png')} />
+      <Image style={styles.header} source={logo} />
       <View style={styles.mainBlock}>
         <Image
           style={styles.img}
@@ -35,36 +37,46 @@ console.log(id)
         />
         <View>
           <Text style={styles.name}>{name}</Text>
-          <AntDesign
-            name="hearto"
-            size={15}
-            color="white"
-            style={styles.icon}
-          />
+          {!isFavorite ? (
+            <TouchableOpacity
+              style={styles.icon}
+              onPress={() => {
+                dispatch(toggleFav(id));
+                setIsFavorite(!isFavorite);
+              }}
+            >
+              <AntDesign name="hearto" size={17} color="white" />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              style={styles.icon}
+              onPress={() => {
+                dispatch(toggleFav(id));
+                setIsFavorite(!isFavorite);
+              }}
+            >
+              <AntDesign name="heart" size={17} color="red" />
+            </TouchableOpacity>
+          )}
         </View>
       </View>
       <ScrollView>
         <Text style={styles.description}>{description}</Text>
-        <Text style={styles.name}>
-          COMICS
-        </Text>
-        {comics && comics.map((comic)=>(
-          <View
-            key={comic.id}
-            style={styles.button}
-          >
-            <Image
-              style={styles.imgCom}
-              source={{
-                uri: comic.thumbnail.path + '.' + comic.thumbnail.extension,
-              }}
-            />
-            <ScrollView>
-              <Text style={styles.text}>{comic.title}</Text>
-              <Text style={styles.text}>{comic.format}</Text>
-            </ScrollView>
-          </View>
-        ))}
+        <Text style={styles.name}>COMICS</Text>
+        {comics &&
+          comics.map((comic) => (
+            <View key={comic.id} style={styles.button}>
+              <Image
+                style={styles.imgCom}
+                source={{
+                  uri: comic.thumbnail.path + '.' + comic.thumbnail.extension,
+                }}
+              />
+              <ScrollView>
+                <Text style={styles.text}>{comic.description}</Text>
+              </ScrollView>
+            </View>
+          ))}
       </ScrollView>
     </SafeAreaView>
   );
