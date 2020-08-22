5b891cc7818825be69821298a8a3a9c31e7bbe5f
dzinrai <dzinrai@mail.ru>
2020-08-22 21:00:45 +0300

    Remove second source of truth, refactor search call and delete unused variables

--- 08-react-2/classworks/Malkov_Aleksei/src/components/header/Header.js
+++ 08-react-2/classworks/Malkov_Aleksei/src/components/header/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
         <Logo className={styles.logo} />
         <SearchBar submit={props.submit} isSearchPage={props.isSearchPage} search={props.search} />
       </div>
-      <div className={styles.header__color_line}>{''}</div>
+      <div className={styles.header__color_line} />
     </header>
   );
 }
