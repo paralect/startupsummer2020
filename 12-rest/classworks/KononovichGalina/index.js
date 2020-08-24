254237b48a38ec034de1e4dd0c8dfe676c9efe18
Galina Kononovich <kononovich1703@gmail.com>
2020-08-24 20:31:39 +0300

    add interviews

--- 12-rest/classworks/KononovichGalina/index.js
+++ 12-rest/classworks/KononovichGalina/index.js
@@ -10,10 +10,14 @@ const app = new koa();
 const public = new Router();
 const privat = new Router();
 
+
 app.use(bodyParser());
 
 let data = [];
-// let slogans=[];
+const slogansCollection = JSON.parse(fs.readFileSync('slogans.json'));
+const topicsCollection = JSON.parse(fs.readFileSync('topics.json'));
+const interviewsCollection = JSON.parse(fs.readFileSync('interviews.json'));
+
 function getPasswordHash(allData) {
   const key = crypto.pbkdf2Sync(allData.password, 'salt', 100000, 64, 'sha512').toString('hex');
   user ={hash: key, login:allData.login};
@@ -23,7 +27,7 @@ function getPasswordHash(allData) {
 public.post('/reg', async (ctx) => {
   ctx.response.body = 'Thank you for registration!';
   data.push(getPasswordHash(ctx.request.body));
-  const usersJson = JSON.stringify(data);
+  const usersJson = JSON.stringify(data, null, 2,);
   fs.writeFile("users.json", usersJson, function(err) {
     if(err) {
         return console.log(err);
@@ -52,31 +56,76 @@ public.post('/login', async (ctx) => {
  }
 });
 
-const slogansCollection = JSON.parse(fs.readFileSync('slogans.json'));
-privat.post('/slogans', async (ctx)  => {
-  const phrase = ctx.request.body;
-  const generatedSlogan = await axios.post('https://models.dobro.ai/gpt2/medium/', {
+public.get('/slogans', async (ctx) => {
+  ctx.body = {
+    data: slogansCollection,
+  };
+})
+
+public.get('/interviews', async (ctx) => {
+  ctx.body = {
+    data: interviewsCollection,
+  };
+})
+
+public.get('/topics', async (ctx) => {
+  ctx.body = {
+    data: topicsCollection,
+  };
+})
+
+
+const responsePorfirevich = async(phrase,length, num_samples) => await axios.post('https://models.dobro.ai/gpt2/medium/', {
     prompt: JSON.stringify(phrase),
-    length: 5,
-    num_samples: 1,
+    length: length,
+    num_samples: num_samples,
   }, {
     headers: { 'User-Agent': 'insomnia/2020.3.3' },
   });
 
+
+const generate = async (ctx, length, num_samples, collection, doc, field) => {
+  
+  const phrase = ctx.request.body;
+  const interview = [];
+  let generatedText;
+
+  if(phrase.questions) {
+    for(const question of phrase.questions) {
+      generatedText = await responsePorfirevich(phrase, length, num_samples);
+      interview.push(question, generatedText.data.replies[0]);
+      interviewsCollection.interviews.push(interview);
+    }
+    console.log(interview);
+  } else {
+    generatedText = await responsePorfirevich(phrase, length, num_samples);
+    collection[field].push({phrase, replies: generatedText.data.replies});
+  }
+ 
   ctx.body = { 
-    data: generatedSlogan.data.replies,
+    data: generatedText.data.replies,
   };
 
-  slogansCollection.slogans.push({phrase, replies: generatedSlogan.data.replies});
 
-  fs.writeFile("slogans.json", JSON.stringify(slogansCollection, null, 2), function(err) {
+  fs.writeFile(doc, JSON.stringify(collection, null, 2), function(err) {
     if(err) {
         return console.log(err);
     }
   console.log("The file was saved!");
   });
+}
+
+privat.post('/slogans', async (ctx)  => {
+  generate(ctx, 5, 1, slogansCollection, "slogans.json", "slogans");
 });
 
+privat.post('/topics', async (ctx) => {
+  generate(ctx, 30, 3, topicsCollection, "topics.json", "topics");
+})
+
+privat.post('/interviews', async (ctx) => {
+  generate(ctx, 10, 1, interviewsCollection, "interviews.json", "interviews");
+})
 
 app
   .use(public.routes())
